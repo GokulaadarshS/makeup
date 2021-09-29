cbc24d0776ea55c3640f7e737fcb69b00c35f6e2
Gokulaadarsh S <88222086+GokulaadarshS@users.noreply.github.com>
2021-09-29 10:00:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+async function makeup(){
+    const url = "http://makeup-api.herokuapp.com/api/v1/products.json";
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        getdata(data);
+    })
+    .catch(function(err){
+        console.log(err.message);
+    });
+}
+
+function getdata(data){
+   let userdiv = document.querySelector("#data");
+   userdiv.innerHTML = "";
+   let table = document.createElement("table");
+
+   data.forEach(function(currentdata){
+       let row = table.insertRow();
+       let name = row.insertCell();
+       name.innerHTML = currentdata.name;
+       let brand = row.insertCell();
+       brand.innerHTML = currentdata.brand;
+       let price = row.insertCell();
+       price.innerHTML = currentdata.price;
+       let image = row.insertCell();
+       image.innerHTML = currentdata.image_link;
+       let product_link = row.insertCell();
+       product_link.innerHTML = currentdata.product_link;
+       let description = row.insertCell();
+       description.innerHTML = currentdata.description;
+   });
+
+   userdiv.appendChild(table)
+
+}
